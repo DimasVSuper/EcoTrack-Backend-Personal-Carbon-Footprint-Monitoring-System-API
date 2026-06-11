e2c5bf3c8e558d53720fb6eb45e964988337f211
DimasVSuper <193190491+DimasVSuper@users.noreply.github.com>
2026-06-11 20:48:37 +0700

    API routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ElectricityLogController;
+use App\Http\Controllers\TransportLogController;
+use App\Http\Controllers\Auth\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Auth
+    Route::get('/user/profile', [AuthController::class, 'profile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Modul Transportasi
+    Route::get('/transport-logs', [TransportLogController::class, 'index']);
+    Route::post('/transport-logs', [TransportLogController::class, 'store']);
+
+    // Modul Listrik
+    Route::get('/electricity-logs', [ElectricityLogController::class, 'index']);
+    Route::post('/electricity-logs', [ElectricityLogController::class, 'store']);
+});
